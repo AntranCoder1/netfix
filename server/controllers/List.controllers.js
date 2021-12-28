1a0bc5d04cfa44ce8a860f7705a01a49469c2f31
thanhantran1 <thanhantran21@gmail.com>
2021-12-28 16:28:36 +0700

    Shorten code in List.routes

--- server/controllers/List.controllers.js
+++ server/controllers/List.controllers.js
@@ -45,3 +45,33 @@ module.exports.deletedListMovie = async (req, res) => {
         res.status(403).json("You are not allowed!");
     }
 };
+
+module.exports.getListMovie = async (req, res) => {
+    const typeQuery = req.query.type;
+    const genreQuery = req.query.genre;
+
+    let list = [];
+
+    try {
+        if (typeQuery) {
+            if (genreQuery) {
+                list = await List.aggregate([
+                    { $sample: { size: 10 } },
+                    { $match: { type: typeQuery, genre: genreQuery } },
+                ]);
+            } else {
+                list = await List.aggregate([
+                    { $sample: { size: 10 } },
+                    { $match: { type: typeQuery } },
+                ]);
+            }
+        } else {
+            list = await List.aggregate([
+                { $sample: { size: 10 } },
+            ]);
+        }
+        res.status(200).json(list);
+    } catch (error) {
+        res.status(500).json("Internal server error");
+    }
+};
--- server/routes/List.routes.js
+++ server/routes/List.routes.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express();
-const List = require('../models/List.models');
 const verify = require('../verifyToken');
 
 const listControllers = require('../controllers/List.controllers');
@@ -23,34 +22,6 @@ router.delete("/:id", verify, listControllers.deletedListMovie);
 // @routes api/lists
 // @desc get lists movie
 // @access private
-router.get("/", verify, async (req, res) => {
-    const typeQuery = req.query.type;
-    const genreQuery = req.query.genre;
-
-    let list = [];
-
-    try {
-        if (typeQuery) {
-            if (genreQuery) {
-                list = await List.aggregate([
-                    { $sample: { size: 10 } },
-                    { $match: { type: typeQuery, genre: genreQuery } },
-                ]);
-            } else {
-                list = await List.aggregate([
-                    { $sample: { size: 10 } },
-                    { $match: { type: typeQuery } },
-                ]);
-            }
-        } else {
-            list = await List.aggregate([
-                { $sample: { size: 10 } },
-            ]);
-        }
-        res.status(200).json(list);
-    } catch (error) {
-        res.status(500).json("Internal server error");
-    }
-});
+router.get("/", verify, listControllers.getListMovie);
 
 module.exports = router;
