71ab2895fb121f25b98d564bd59f29aa8d12c1e2
thanhantran1 <thanhantran21@gmail.com>
2022-01-06 14:42:45 +0700

    Update chart user analytics in home on admin side

--- admin/src/pages/Home/Home.js
+++ admin/src/pages/Home/Home.js
@@ -1,16 +1,53 @@
-import React from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import Chart from '../chart/Chart';
 import FeaturedInfo from '../featuredInfo/FeaturedInfo';
 import { data } from '../../data';
 import './Home.css';
 import WidgetSm from '../widgetSm/WidgetSm';
 import Widgetlg from '../Widgetlg/Widgetlg';
+import axios from 'axios';
 
 const Home = () => {
+
+    const [userStats, setUserStats] = useState([]);
+
+    const MONTHS = useMemo(
+        () => [
+            "Jan",
+            "Feb",
+            "Mar",
+            "Apr",
+            "May",
+            "Jun",
+            "Jul",
+            "Agu",
+            "Sep",
+            "Oct",
+            "Nov",
+            "Dec",
+        ], 
+        []
+    );
+
+    useEffect(() => {
+        const getUserStats = async () => {
+            try {
+                const res = await axios.get("/users/stats");
+                res.data.map((item) => 
+                    setUserStats((prev) => [
+                        ...prev,
+                        { name: MONTHS[item._id - 1], "Active User": item.total },
+                    ])
+                );
+            } catch (error) {}
+        };
+        getUserStats();
+    }, [MONTHS]);
+
     return (
         <div className="home">
             <FeaturedInfo />
-            <Chart data={data} title="User Analytics" grid datakey="uv" />
+            <Chart data={userStats} title="User Analytics" grid datakey="Active User" />
             <div className="homwWidgets">
                 <WidgetSm />
                 <Widgetlg />
