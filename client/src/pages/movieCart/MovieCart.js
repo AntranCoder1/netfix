ffb22d5af13e376049b1a43d35521b17b83a924d
thanhantran1 <thanhantran21@gmail.com>
2022-02-25 18:12:29 +0700

    Add new page movie like cart

--- client/src/pages/movieCart/MovieCart.js
+++ client/src/pages/movieCart/MovieCart.js
@@ -1,10 +1,24 @@
-import React, { useState } from 'react';
+import React, { useEffect } from 'react';
 import './MovieCart.scss';
 import NavBar from '../../components/navBar/NavBar';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import { useSelector } from 'react-redux';
 
 const MovieCart = () => {
+
+    const user = useSelector(state => state.user.currentUser);
+    const movies = useSelector(state => state.movie.movies);
+
+    const check = movies.map((movie) => {
+        if (movie.likers.includes(user._id)) {
+            return movie;
+        }
+    })
+
+    const filtered = check.filter(function(el) {
+        return el !== undefined
+    });
     
     return (
         <div className="movie-cart">
@@ -12,130 +26,37 @@ const MovieCart = () => {
             <div className="movie-container">
                 <div className="left">
                     <p className="left-title">Video đã thích</p>
-                    <p style={{ fontSize: "15px", color: "#ccc" }}><span>0</span> Video</p>
+                    <p style={{ fontSize: "15px", color: "#ccc" }}>
+                        <span>{filtered.length} </span> 
+                        Video
+                    </p>
                     <div className="left-security">
                         <LockOutlinedIcon className="left-icon" />
                         <p style={{ fontSize: "13px", fontWeight: "bold", color: "#e0e0d1" }}>Riêng tư</p>
                     </div>
                     <div className="left-bottom"></div>
                     <div className="left-user">
-                        <img src="https://i.pinimg.com/564x/d5/c8/46/d5c846335bdf975a6ab1bd1396616549.jpg" alt="" />
-                        <p className="left-user-name">Hoàng Châu</p>
+                        <img src={user.picture} alt="" />
+                        <p className="left-user-name">{user.name}</p>
                     </div>
                 </div>
                 <div className="right">
-                    <div className="right-right">
-                        <div className="right-content">
-                            <span style={{ fontSize: "20px" }}>0</span>
-                            <div className="right-title">
-                                <img src="https://i.pinimg.com/564x/41/a3/d5/41a3d537a95e6d83edb4409ce51997e4.jpg" alt="" />
-                                <div className="right-genre">
-                                    <p style={{ fontWeight: "500" }}>Expression statement: produces a value and performs an action</p>
-                                    <p style={{ fontSize: "15px", color: "#ccc", marginTop: "20px" }}>Animation</p>
+                    { filtered.map((movie) => (
+                        <div className="right-right">
+                            <div className="right-content">
+                                <span style={{ fontSize: "20px" }}>0</span>
+                                <div className="right-title">
+                                    <img src={movie.img} alt="" />
+                                    <div className="right-genre">
+                                        <p style={{ fontWeight: "500" }}>{movie.title}</p>
+                                        <p style={{ fontSize: "15px", color: "#ccc", marginTop: "20px" }}>{movie.genre}</p>
+                                    </div>
                                 </div>
+                                <MoreVertIcon className="right-icon" />
                             </div>
-                            <MoreVertIcon className="right-icon" />
+                            <div className="right-bottom"></div>
                         </div>
-                        <div className="right-bottom"></div>
-                    </div>
-                    <div className="right-right">
-                        <div className="right-content">
-                            <span style={{ fontSize: "20px" }}>0</span>
-                            <div className="right-title">
-                                <img src="https://i.pinimg.com/564x/41/a3/d5/41a3d537a95e6d83edb4409ce51997e4.jpg" alt="" />
-                                <div className="right-genre">
-                                    <p style={{ fontWeight: "500" }}>Expression statement: produces a value and performs an action</p>
-                                    <p style={{ fontSize: "15px", color: "#ccc", marginTop: "20px" }}>Animation</p>
-                                </div>
-                            </div>
-                            <MoreVertIcon className="right-icon" />
-                        </div>
-                        <div className="right-bottom"></div>
-                    </div>
-                    <div className="right-right">
-                        <div className="right-content">
-                            <span style={{ fontSize: "20px" }}>0</span>
-                            <div className="right-title">
-                                <img src="https://i.pinimg.com/564x/41/a3/d5/41a3d537a95e6d83edb4409ce51997e4.jpg" alt="" />
-                                <div className="right-genre">
-                                    <p style={{ fontWeight: "500" }}>Expression statement: produces a value and performs an action</p>
-                                    <p style={{ fontSize: "15px", color: "#ccc", marginTop: "20px" }}>Animation</p>
-                                </div>
-                            </div>
-                            <MoreVertIcon className="right-icon" />
-                        </div>
-                        <div className="right-bottom"></div>
-                    </div>
-                    <div className="right-right">
-                        <div className="right-content">
-                            <span style={{ fontSize: "20px" }}>0</span>
-                            <div className="right-title">
-                                <img src="https://i.pinimg.com/564x/41/a3/d5/41a3d537a95e6d83edb4409ce51997e4.jpg" alt="" />
-                                <div className="right-genre">
-                                    <p style={{ fontWeight: "500" }}>Expression statement: produces a value and performs an action</p>
-                                    <p style={{ fontSize: "15px", color: "#ccc", marginTop: "20px" }}>Animation</p>
-                                </div>
-                            </div>
-                            <MoreVertIcon className="right-icon" />
-                        </div>
-                        <div className="right-bottom"></div>
-                    </div>
-                    <div className="right-right">
-                        <div className="right-content">
-                            <span style={{ fontSize: "20px" }}>0</span>
-                            <div className="right-title">
-                                <img src="https://i.pinimg.com/564x/41/a3/d5/41a3d537a95e6d83edb4409ce51997e4.jpg" alt="" />
-                                <div className="right-genre">
-                                    <p style={{ fontWeight: "500" }}>Expression statement: produces a value and performs an action</p>
-                                    <p style={{ fontSize: "15px", color: "#ccc", marginTop: "20px" }}>Animation</p>
-                                </div>
-                            </div>
-                            <MoreVertIcon className="right-icon" />
-                        </div>
-                        <div className="right-bottom"></div>
-                    </div>
-                    <div className="right-right">
-                        <div className="right-content">
-                            <span style={{ fontSize: "20px" }}>0</span>
-                            <div className="right-title">
-                                <img src="https://i.pinimg.com/564x/41/a3/d5/41a3d537a95e6d83edb4409ce51997e4.jpg" alt="" />
-                                <div className="right-genre">
-                                    <p style={{ fontWeight: "500" }}>Expression statement: produces a value and performs an action</p>
-                                    <p style={{ fontSize: "15px", color: "#ccc", marginTop: "20px" }}>Animation</p>
-                                </div>
-                            </div>
-                            <MoreVertIcon className="right-icon" />
-                        </div>
-                        <div className="right-bottom"></div>
-                    </div>
-                    <div className="right-right">
-                        <div className="right-content">
-                            <span style={{ fontSize: "20px" }}>0</span>
-                            <div className="right-title">
-                                <img src="https://i.pinimg.com/564x/41/a3/d5/41a3d537a95e6d83edb4409ce51997e4.jpg" alt="" />
-                                <div className="right-genre">
-                                    <p style={{ fontWeight: "500" }}>Expression statement: produces a value and performs an action</p>
-                                    <p style={{ fontSize: "15px", color: "#ccc", marginTop: "20px" }}>Animation</p>
-                                </div>
-                            </div>
-                            <MoreVertIcon className="right-icon" />
-                        </div>
-                        <div className="right-bottom"></div>
-                    </div>
-                    <div className="right-right">
-                        <div className="right-content">
-                            <span style={{ fontSize: "20px" }}>0</span>
-                            <div className="right-title">
-                                <img src="https://i.pinimg.com/564x/41/a3/d5/41a3d537a95e6d83edb4409ce51997e4.jpg" alt="" />
-                                <div className="right-genre">
-                                    <p style={{ fontWeight: "500" }}>Expression statement: produces a value and performs an action</p>
-                                    <p style={{ fontSize: "15px", color: "#ccc", marginTop: "20px" }}>Animation</p>
-                                </div>
-                            </div>
-                            <MoreVertIcon className="right-icon" />
-                        </div>
-                        <div className="right-bottom"></div>
-                    </div>
+                    )) }
                 </div>
             </div>
         </div>
