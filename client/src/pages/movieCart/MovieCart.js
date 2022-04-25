5a804a907db2a423da94e65bbf845e4867b2357c
thanhantran1 <thanhantran21@gmail.com>
2022-04-25 10:49:23 +0700

    Add function search on likeMovieG

--- client/src/pages/movieCart/MovieCart.js
+++ client/src/pages/movieCart/MovieCart.js
@@ -6,6 +6,7 @@ import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 import SkeletonLike from '../../components/skeleton/SkeletonLike';
+import MovieItem from '../../components/MovieItem/MovieItem';
 
 const MovieCart = () => {
 
@@ -14,6 +15,7 @@ const MovieCart = () => {
     const [users, setUsers] = useState([]);
     const [filtered, setFiltered] = useState([]);
     const [loading, setLoading] = useState(false);
+    const [movieSearch, setMovieSearch] = useState([]);
 
     useEffect(() => {
         setLoading(true);
@@ -52,59 +54,82 @@ const MovieCart = () => {
         getCurrentUser();
     }, []);
 
+    const search = async (searchValue) => {
+        try {
+            const res = await axios.get(`/movies/search?value=${searchValue}`, {
+                headers: {
+                    token: "Bearer " + user.token
+                }
+            })
+            setMovieSearch(res.data);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
     useEffect(() => {
         document.title = 'Netflix - liked video'
     }, []);
 
     return (
         <div className="movie-cart">
-            <NavBar />
-            { loading && <SkeletonLike /> }
-            { !loading &&
-                <div className="movie-container">
-                    <div className="left">
-                        <p className="left-title">Video đã thích</p>
-                        <p style={{ fontSize: "15px", color: "#ccc" }}>
-                            <span>{check.length} </span> 
-                            Video
-                        </p>
-                        <div className="left-security">
-                            <LockOutlinedIcon className="left-icon" />
-                            <p style={{ fontSize: "13px", fontWeight: "bold", color: "#e0e0d1" }}>Riêng tư</p>
-                        </div>
-                        <div className="left-bottom"></div>
-                        <div className="left-user">
-                            <img src={users.picture} alt="" />
-                            <p className="left-user-name">{users.name}</p>
-                        </div>
-                    </div>
-                    <div className="right">
-                        { check.map((movie) => (
-                            <div className="right-right">
-                                <div className="right-content">
-                                    <span style={{ fontSize: "20px" }}>
-                                        { check.indexOf(movie) + 1 }
-                                    </span>
-                                    <Link to={`/watch/${movie._id}`}>
-                                        <>
-                                            <div className="right-title">
-                                                <img src={movie.img} alt="" />
-                                                <div className="right-genre">
-                                                    <p style={{ fontWeight: "500" }}>{movie.title}</p>
-                                                    <p style={{ fontSize: "15px", color: "#ccc", marginTop: "20px" }}>
-                                                        {movie.genre}
-                                                    </p>
-                                                </div>
-                                            </div>
-                                        </>
-                                    </Link>
+            <NavBar search={search} />
+            { movieSearch.length === 0 ? (
+                <>
+                    { loading && <SkeletonLike /> }
+                    { !loading &&
+                        <div className="movie-container">
+                            <div className="left">
+                                <p className="left-title">Video đã thích</p>
+                                <p style={{ fontSize: "15px", color: "#ccc" }}>
+                                    <span>{check.length} </span> 
+                                    Video
+                                </p>
+                                <div className="left-security">
+                                    <LockOutlinedIcon className="left-icon" />
+                                    <p style={{ fontSize: "13px", fontWeight: "bold", color: "#e0e0d1" }}>Riêng tư</p>
                                 </div>
-                                <div className="right-bottom"></div>
+                                <div className="left-bottom"></div>
+                                <div className="left-user">
+                                    <img src={users.picture} alt="" />
+                                    <p className="left-user-name">{users.name}</p>
+                                </div>
                             </div>
-                        )) }
-                    </div>
+                            <div className="right">
+                                { check.map((movie) => (
+                                    <div className="right-right">
+                                        <div className="right-content">
+                                            <span style={{ fontSize: "20px" }}>
+                                                { check.indexOf(movie) + 1 }
+                                            </span>
+                                            <Link to={`/watch/${movie._id}`}>
+                                                <>
+                                                    <div className="right-title">
+                                                        <img src={movie.img} alt="" />
+                                                        <div className="right-genre">
+                                                            <p style={{ fontWeight: "500" }}>{movie.title}</p>
+                                                            <p style={{ fontSize: "15px", color: "#ccc", marginTop: "20px" }}>
+                                                                {movie.genre}
+                                                            </p>
+                                                        </div>
+                                                    </div>
+                                                </>
+                                            </Link>
+                                        </div>
+                                        <div className="right-bottom"></div>
+                                    </div>
+                                )) }
+                            </div>
+                        </div>
+                    }
+                </>
+            ) : (
+                <div className="movie-search">
+                    { movieSearch.map((item, i) => (
+                        <MovieItem key={item._id} index={i} movie={item} />
+                    )) }
                 </div>
-            }
+            ) }
         </div>
     )
 }
